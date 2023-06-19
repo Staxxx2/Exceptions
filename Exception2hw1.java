fd5bb440119217ae32e543e680ab0afd517938d3
StaxxxInc <sersersin2@gmail.com>
2023-06-19 23:41:15 +0300

    Hw 2.1 done

---
+++ Exception2hw1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exception2hw1 {
+
+public static void main(String[] args) {
+    float userNumber = getFractionalInput();
+    System.out.println("Пожалуйста, введите допустимое дробное число: " + userNumber);
+}
+public static float getFractionalInput() {
+    Scanner scanner = new Scanner(System.in);
+    float input = 0.0f;
+    while (true) {
+        System.out.print("Please enter a fractional number: ");
+        if (scanner.hasNextFloat()) {
+            input = scanner.nextFloat();
+            break;
+        } else {
+            System.out.println("Неверный Ввод. Пожалуйста, введите допустимое дробное число");
+            scanner.next();
+        }
+    }
+    return input;
+}
+}
